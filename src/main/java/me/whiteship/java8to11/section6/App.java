27b2ccda87e88bf8dadcc57dc8e44b468b82a6a4
JinJungs <nehciyz@gmail.com>
2024-03-25 23:59:09 +0900

    [섹션6] 자바 Concurrent 프로그래밍

---
+++ src/main/java/me/whiteship/java8to11/section6/App.java
@@ -0,0 +1,42 @@
+package me.whiteship.java8to11.section6;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        /* 방법 1. 클래스로 쓰레드 만들기 */
+        // MyThread myThread = new MyThread();
+        // myThread.start();
+
+        /* 방법 2. 람다로 쓰레드 만들기 */
+        Thread thread = new Thread(() -> {
+            while (true) {
+                System.out.println("Thread: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    System.out.println("interrupt!");
+                    return;
+                }
+            }
+        });
+        thread.start();
+
+        System.out.println("Hello: " + Thread.currentThread().getName());
+        /* (1) sleep */
+        // Thread.sleep(3000L);
+        /* (2) interrupt */
+        // thread.interrupt();
+        /* (3) join - 기다림 */
+        thread.join();
+
+        /* Thread의 순서는 보장하지 못한다 */
+        // Hello
+        // Thread: Thread-0
+    }
+
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Thread: " + Thread.currentThread().getName());
+        }
+    }
+}
